43ac55c904bb229d439b3becbd69b6a1a375af3c
Goberomsu <310o@naver.com>
2020-07-18 14:38:22 +0900

    Timer 기능 구현(직접 구현)

--- app/src/main/java/com/e/tostarttimer/MainActivity.kt
+++ app/src/main/java/com/e/tostarttimer/MainActivity.kt
@@ -23,43 +23,93 @@ class MainActivity : AppCompatActivity() {
     // 깔끔하게 다이얼로그로 입력하게 바꾸기
     // 3차 목표
     // 그래픽으로 카운트 다운 표시하기
-    var sumOfTime: Int? = 0
+    var sumOfTime: Int = 0
+    var timerTask: Timer? = null
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        setTimer()
+//
+//        val timePicker = MyTimePickerDialog(
+//            this,
+//            MyTimePickerDialog.OnTimeSetListener() { timePicker: TimePicker, hoursOfDay: Int, minute: Int, seconds: Int ->
+//
+////                timeText.setText(
+////                    String.format("%02d", hoursOfDay) +
+////                            ":" + String.format("%02d", minute) +
+////                            ":" + String.format("%02d", seconds)
+////                )
+//                sumOfTime = hoursOfDay * 60 * 60 + minute * 60 + seconds
+//                var Timer = object : CountDownTimer(sumOfTime!!.toLong(), 100) {
+//                    override fun onTick(millisUntilFinished: Long) {
+//                        timeText.setText("${millisUntilFinished} 남았습니다")
+//                    }
+//
+//                    override fun onFinish() {
+//                        Toast.makeText(this@MainActivity, "CountDown Finished.", Toast.LENGTH_SHORT)
+//                            .show()
+//                    }
+//                }
+//                Timer.start()
+//                startTimer()
+//            },
+//            Calendar.HOUR_OF_DAY,
+//            Calendar.MINUTE,
+//            Calendar.SECOND,
+//            true
+//        )
 
+//        timePicker.show()
+
+    }
+
+    fun setTimer() {
         val timePicker = MyTimePickerDialog(
             this,
             MyTimePickerDialog.OnTimeSetListener() { timePicker: TimePicker, hoursOfDay: Int, minute: Int, seconds: Int ->
-
-//                timeText.setText(
-//                    String.format("%02d", hoursOfDay) +
-//                            ":" + String.format("%02d", minute) +
-//                            ":" + String.format("%02d", seconds)
-//                )
                 sumOfTime = hoursOfDay * 60 * 60 + minute * 60 + seconds
-                var Timer = object : CountDownTimer(sumOfTime!!.toLong(), 100) {
-                    override fun onTick(millisUntilFinished: Long) {
-                        timeText.setText("${millisUntilFinished} 남았습니다")
-                    }
-
-                    override fun onFinish() {
-                        Toast.makeText(this@MainActivity, "CountDown Finished.", Toast.LENGTH_SHORT)
-                            .show()
-                    }
-                }
-                Timer.start()
+                storeTime(sumOfTime)
+                startTimer()
             },
             Calendar.HOUR_OF_DAY,
             Calendar.MINUTE,
             Calendar.SECOND,
             true
         )
-
         timePicker.show()
+    }
 
+    fun startTimer() {
+
+//        var Timer = object : CountDownTimer(sumOfTime.toLong(), 1000) {
+//            override fun onTick(millisUntilFinished: Long) {
+//                timeText.setText("${millisUntilFinished/1000} 남았습니다")
+//            }
+//
+//            override fun onFinish() {
+//                Toast.makeText(this@MainActivity, "CountDown Finished.", Toast.LENGTH_SHORT)
+//                    .show()
+//            }
+//        }
+//        Timer.start()
+        var countDownTime = sumOfTime
+        timerTask = timer(period = 1000) {
+
+            countDownTime--
+
+            runOnUiThread { timeText.setText("$countDownTime") }
+        }
+    }
+
+
+    fun storeTime(Time: Int): Int {
+        sumOfTime = Time
+        Toast.makeText(this@MainActivity, "${sumOfTime}은 storeTime에서 호출됨", Toast.LENGTH_SHORT)
+            .show()
+        return sumOfTime
     }
 
 
