23ed9d4cf987a2d20ddba3d4d022659855da1656
Goberomsu <310o@naver.com>
2020-07-18 13:41:01 +0900

    Timer 기능 구현

--- app/src/main/java/com/e/tostarttimer/MainActivity.kt
+++ app/src/main/java/com/e/tostarttimer/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.e.tostarttimer
 
+import android.content.Context
 import android.os.Build
 import android.os.Bundle
 import android.os.CountDownTimer
+import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import com.ikovac.timepickerwithseconds.MyTimePickerDialog
@@ -10,6 +12,7 @@ import com.ikovac.timepickerwithseconds.TimePicker
 import kotlinx.android.synthetic.main.activity_main.*
 import java.time.LocalDate
 import java.util.*
+import kotlin.concurrent.timer
 
 
 class MainActivity : AppCompatActivity() {
@@ -20,11 +23,7 @@ class MainActivity : AppCompatActivity() {
     // 깔끔하게 다이얼로그로 입력하게 바꾸기
     // 3차 목표
     // 그래픽으로 카운트 다운 표시하기
-
-    val Timer: CountDownTimer? = null
-
-    @RequiresApi(Build.VERSION_CODES.O)
-    var now = LocalDate.now()
+    var sumOfTime: Int? = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,11 +34,23 @@ class MainActivity : AppCompatActivity() {
             this,
             MyTimePickerDialog.OnTimeSetListener() { timePicker: TimePicker, hoursOfDay: Int, minute: Int, seconds: Int ->
 
-                timeText.setText(
-                     String.format("%02d", hoursOfDay) +
-                            ":" + String.format("%02d", minute) +
-                            ":" + String.format("%02d", seconds)
-                );
+//                timeText.setText(
+//                    String.format("%02d", hoursOfDay) +
+//                            ":" + String.format("%02d", minute) +
+//                            ":" + String.format("%02d", seconds)
+//                )
+                sumOfTime = hoursOfDay * 60 * 60 + minute * 60 + seconds
+                var Timer = object : CountDownTimer(sumOfTime!!.toLong(), 100) {
+                    override fun onTick(millisUntilFinished: Long) {
+                        timeText.setText("${millisUntilFinished} 남았습니다")
+                    }
+
+                    override fun onFinish() {
+                        Toast.makeText(this@MainActivity, "CountDown Finished.", Toast.LENGTH_SHORT)
+                            .show()
+                    }
+                }
+                Timer.start()
             },
             Calendar.HOUR_OF_DAY,
             Calendar.MINUTE,
@@ -51,13 +62,5 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-    private fun startTimer() {
 
-    }
-
-    private fun stopTimer() {
-
-    }
-
-    private fun drawTimer() {}
 }
