fcf2641b74137b4e8cde332b617ca5dae57bd9ea
Goberomsu <310o@naver.com>
2020-07-18 12:51:34 +0900

    timePicker second 추가

--- app/src/main/java/com/e/tostarttimer/MainActivity.kt
+++ app/src/main/java/com/e/tostarttimer/MainActivity.kt
@@ -1,8 +1,16 @@
 package com.e.tostarttimer
 
-import androidx.appcompat.app.AppCompatActivity
+import android.os.Build
 import android.os.Bundle
 import android.os.CountDownTimer
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
+import com.ikovac.timepickerwithseconds.MyTimePickerDialog
+import com.ikovac.timepickerwithseconds.TimePicker
+import kotlinx.android.synthetic.main.activity_main.*
+import java.time.LocalDate
+import java.util.*
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,9 +23,32 @@ class MainActivity : AppCompatActivity() {
 
     val Timer: CountDownTimer? = null
 
+    @RequiresApi(Build.VERSION_CODES.O)
+    var now = LocalDate.now()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        val timePicker = MyTimePickerDialog(
+            this,
+            MyTimePickerDialog.OnTimeSetListener() { timePicker: TimePicker, hoursOfDay: Int, minute: Int, seconds: Int ->
+
+                timeText.setText(
+                     String.format("%02d", hoursOfDay) +
+                            ":" + String.format("%02d", minute) +
+                            ":" + String.format("%02d", seconds)
+                );
+            },
+            Calendar.HOUR_OF_DAY,
+            Calendar.MINUTE,
+            Calendar.SECOND,
+            true
+        )
+
+        timePicker.show()
+
     }
 
     private fun startTimer() {
