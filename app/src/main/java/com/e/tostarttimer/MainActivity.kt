11913c26d23ff822d2df4942adfe88630a8dabd5
Goberomsu <310o@naver.com>
2020-07-29 00:17:12 +0900

    성공. 텍스트 출력 수정 예정

--- app/src/main/java/com/e/tostarttimer/MainActivity.kt
+++ app/src/main/java/com/e/tostarttimer/MainActivity.kt
@@ -1,47 +1,65 @@
 package com.e.tostarttimer
 
 
+import android.graphics.Color
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.ikovac.timepickerwithseconds.MyTimePickerDialog
 import com.ikovac.timepickerwithseconds.TimePicker
-import com.uzairiqbal.circulartimerview.CircularTimerListener
-import com.uzairiqbal.circulartimerview.TimeFormatEnum
+import com.sfyc.ctpv.CountTimeProgressView
+
 import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
+import kotlin.concurrent.timer
 
 
 class MainActivity : AppCompatActivity() {
 
-    // 1차 목표
-    // string 입력하면 카운트 다운하기
-    // 2차 목표
-    // 깔끔하게 다이얼로그로 입력하게 바꾸기
-    // 3차 목표
-    // 그래픽으로 카운트 다운 표시하기
+
     var sumOfTime: Int = 0
     var timerTask: Timer? = null
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        with(countTimeProgressView) {
+            startAngle = 0f
+            countTime = 6000L
+            textStyle = CountTimeProgressView.TextStyle.SECOND
+            borderWidth = 4f
+            borderBottomColor = Color.GRAY
+            borderDrawColor = Color.RED
+            backgroundColorCenter = Color.WHITE
+            markBallFlag = true
+            markBallWidth = 6f
+            markBallColor = Color.GREEN
+            titleCenterText = "jump（%s）s"
+            titleCenterTextColor = Color.BLACK
+            titleCenterTextSize = 16f
+            addOnEndListener(object : CountTimeProgressView.OnEndListener {
+                override fun onAnimationEnd() {
+                    Toast.makeText(this@MainActivity, "时间到", Toast.LENGTH_SHORT).show()
+                }
+
+                override fun onClick(overageTime: Long) {
+                    if (countTimeProgressView.isRunning) {
+                        countTimeProgressView.cancelCountTimeAnimation()
+                        Log.e("overageTime", "overageTime = " + overageTime)
+                    } else {
+                        countTimeProgressView.startCountTimeAnimation()
+                    }
+                }
+            })
+        }
+        countTimeProgressView.startCountTimeAnimation()
+
         setTimePicker()
-        progressBar.setCircularTimerListener(object : CircularTimerListener {
-            override fun updateDataOnTick(remainingTimeInMs: Long): String {
-                return Math.ceil((remainingTimeInMs / 1000f).toDouble()).toString()
-            }
 
-            override fun onTimerFinished() {
-                Toast.makeText(this@MainActivity, "FINISHED", Toast.LENGTH_SHORT).show();
-                progressBar.setPrefix("");
-                progressBar.setSuffix("");
-                progressBar.setText("FINISHED THANKS!");
-            }
-
-        }, 10, TimeFormatEnum.SECONDS, 10);
 
     }
 
@@ -50,44 +68,45 @@ class MainActivity : AppCompatActivity() {
             this,
             MyTimePickerDialog.OnTimeSetListener() { timePicker: TimePicker, hoursOfDay: Int, minute: Int, seconds: Int ->
                 sumOfTime = hoursOfDay * 60 * 60 + minute * 60 + seconds
-//                storeTime(sumOfTime)
+                storeTime(sumOfTime)
 //                startTimer()
+                countTimeProgressView.countTime = sumOfTime.toLong() * 1000L
+                countTimeProgressView.startCountTimeAnimation()
             },
             Calendar.HOUR_OF_DAY,
             Calendar.MINUTE,
             Calendar.SECOND,
             true
         )
+
+
         timePicker.show()
+
     }
 
 
-    //
-//    fun startTimer() {
-//
-//
-//        var countDownTime = sumOfTime
-//        var percentage: Float = 100.0f
-//        timerTask = timer(period = 1000) {
-//
-//            countDownTime--
-//
-//            runOnUiThread {
-////                timeText.setText("$countDownTime")
-//
-//            }
-//        }
-//    }
-//
-//
-//    fun storeTime(Time: Int): Int {
-//        sumOfTime = Time
-//        Toast.makeText(this@MainActivity, "${sumOfTime}은 storeTime에서 호출됨", Toast.LENGTH_SHORT)
-//            .show()
-//        return sumOfTime
-//    }
+    fun startTimer() {
+
+        var countDownTime = sumOfTime
+        var percentage: Float = 100.0f
+        timerTask = timer(period = 1000) {
+
+            countDownTime--
+
+            runOnUiThread {
+//                timeText.setText("$countDownTime")
+
+            }
+        }
+    }
 
 
+    fun storeTime(Time: Int): Int {
+        sumOfTime = Time
+        Toast.makeText(this@MainActivity, "${sumOfTime}은 storeTime에서 호출됨", Toast.LENGTH_SHORT)
+            .show()
+        return sumOfTime
+    }
 }
 
 
